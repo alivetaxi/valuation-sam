bbf896201bee45c1ad09400fb39db7b6890aa732
heng <alivetaxi@gmail.com>
2024-05-20 16:33:15 +0800

    calculate ratio and format

--- lambda/get_company_report.py
+++ lambda/get_company_report.py
@@ -1,69 +1,80 @@
 """ This function gets the report from a company. """
 
 from decimal import Decimal
-import time
+from datetime import datetime
 
 import boto3
 import requests
 from bs4 import BeautifulSoup
 
-COMPANY_TABLE_NAME = 'valuation-sam-ListingCompanyTable-LDPLI51PXXDP'
-REPORT_TABLE_NAME = 'valuation-sam-CompanyReportTable-7EAUU6GT8G4Y'
-dynamodb = boto3.resource('dynamodb')
+COMPANY_TABLE_NAME = "valuation-sam-ListingCompanyTable-LDPLI51PXXDP"
+REPORT_TABLE_NAME = "valuation-sam-CompanyReportTable-7EAUU6GT8G4Y"
+RATIO_TABLE_NAME = "valuation-sam-CompanyRatioTable-NPLFQQ8WN4JC"
+dynamodb = boto3.resource("dynamodb")
 company_table = dynamodb.Table(COMPANY_TABLE_NAME)
 report_table = dynamodb.Table(REPORT_TABLE_NAME)
-parse_tables = ['BalanceSheet', 'StatementOfComprehensiveIncome', 'StatementsOfCashFlows']
+ratio_table = dynamodb.Table(RATIO_TABLE_NAME)
+parse_tables = [
+    "BalanceSheet",
+    "StatementOfComprehensiveIncome",
+    "StatementsOfCashFlows",
+]
+
 
 def get_company_to_run():
-    """ This function gets the company to deal with. """
+    """This function gets the company to deal with."""
 
-    companies = company_table.scan()['Items']
-    company_to_run = min(companies, key=lambda c: c['process_time'])
+    companies = company_table.scan()["Items"]
+    company_to_run = min(companies, key=lambda c: c["process_time"])
     print(company_to_run)
     return company_to_run
 
+
 def get_report_records(company_id):
-    """ This function gets reports already in db. """
+    """This function gets reports already in db."""
 
     reports = report_table.query(
-        Select = 'SPECIFIC_ATTRIBUTES',
-        ProjectionExpression = 'year_quarter',
-        KeyConditionExpression = boto3.dynamodb.conditions.Key('company_id').eq(company_id)
-    )['Items']
+        Select="SPECIFIC_ATTRIBUTES",
+        ProjectionExpression="year_quarter",
+        KeyConditionExpression=boto3.dynamodb.conditions.Key("company_id").eq(
+            company_id
+        ),
+    )["Items"]
 
-    return [r['year_quarter'] for r in reports]
+    return [r["year_quarter"] for r in reports]
+
 
 def get_report_paths(company_id, reports):
-    """ This function gets urls for new financial reports. """
+    """This function gets urls for new financial reports."""
 
     resp = requests.post(
-        'https://mops.twse.com.tw/mops/web/ajax_t203sb01',
+        "https://mops.twse.com.tw/mops/web/ajax_t203sb01",
         {
-            'encodeURIComponent': 1,
-            'step': 1,
-            'firstin': 1,
-            'off': 1,
-            'queryName': 'co_id',
-            'inpuType': 'co_id',
-            'TYPEK': 'all',
-            'co_id': company_id
+            "encodeURIComponent": 1,
+            "step": 1,
+            "firstin": 1,
+            "off": 1,
+            "queryName": "co_id",
+            "inpuType": "co_id",
+            "TYPEK": "all",
+            "co_id": company_id,
         },
-        timeout = 30
+        timeout=30,
     )
 
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    trs = soup.select('tr')
+    soup = BeautifulSoup(resp.text, "html.parser")
+    trs = soup.select("tr")
     path_map = {}
     for tr in trs:
-        path_td = tr.select_one(':nth-child(4) > input')
+        path_td = tr.select_one(":nth-child(4) > input")
         if path_td is None:
             continue
 
-        report_path = path_td.attrs['onclick']
+        report_path = path_td.attrs["onclick"]
         if report_path is None:
             continue
 
-        minguo_year_quarter = tr.select_one(':nth-child(1)').get_text()
+        minguo_year_quarter = tr.select_one(":nth-child(1)").get_text()
         if minguo_year_quarter is None or len(minguo_year_quarter) < 5:
             continue
 
@@ -75,62 +86,174 @@ def get_report_paths(company_id, reports):
         if year_quarter in reports:
             continue
 
-        path_map[year_quarter] = report_path[13:len(report_path)-10]
+        path_map[year_quarter] = report_path[13 : len(report_path) - 10]
 
         if len(path_map) == 5:
             break
 
     return path_map
 
+
 def get_report(company_id, report_paths):
-    """ This function parses xbrls and save data to db. """
+    """This function parses xbrls and save data to db."""
 
     for year_quarter, report_path in report_paths.items():
-        report = {
-            'company_id': company_id,
-            'year_quarter': year_quarter
-        }
-        resp = requests.get('https://mops.twse.com.tw' + report_path, timeout = 30)
-        soup = BeautifulSoup(resp.text, 'html.parser')
+        report = {"company_id": company_id, "year_quarter": year_quarter}
+        resp = requests.get("https://mops.twse.com.tw" + report_path, timeout=30)
+        soup = BeautifulSoup(resp.text, "html.parser")
 
         for parse_table in parse_tables:
-            trs = soup.select('#' + parse_table + ' + div + table tr')
+            trs = soup.select("#" + parse_table + " + div + table tr")
             for tr in trs:
-                acc_code = tr.select_one(':nth-child(1)').get_text().strip()
+                acc_code = tr.select_one(":nth-child(1)").get_text().strip()
                 if len(acc_code) == 0 or len(acc_code) > 6:
                     continue
 
-                acc_value = tr.select_one(':nth-child(3)').get_text().strip().replace(',', '')
-                if acc_value.startswith('('):
-                    acc_value = '-' + acc_value[1:len(acc_value)-1]
+                acc_value = (
+                    tr.select_one(":nth-child(3)").get_text().strip().replace(",", "")
+                )
+                if acc_value.startswith("("):
+                    acc_value = "-" + acc_value[1 : len(acc_value) - 1]
 
                 report[acc_code] = Decimal(acc_value)
 
         if len(report) > 2:
-            report_table.put_item(Item = report)
+            report_table.put_item(Item=report)
         else:
-            print(company_id + ' / ' + year_quarter + ' empty report error')
+            print(company_id + " / " + year_quarter + " empty report error")
             print(resp.text)
 
+
+def calculate_company_ratio(curr, beg, last):
+    """This function calculate financial ratios for the given year."""
+
+    if curr is None or beg is None or last is None:
+        return None
+    if curr["1XXX"] is None or beg["1XXX"] is None or last["1XXX"] is None:
+        return None
+
+    ratio = {}
+    ratio["company_id"] = curr["company_id"]
+    ratio["year_quarter"] = curr["year_quarter"]
+
+    curr_fcf = curr["AAAA"] + (curr["B02700"] or 0)
+    beg_fcf = beg["AAAA"] + (beg["B02700"] or 0)
+    last_fcf = last["AAAA"] + (last["B02700"] or 0)
+    ratio["curr_fcf"] = curr_fcf
+    ratio["beg_fcf"] = beg_fcf
+    ratio["last_fcf"] = last_fcf
+    ratio["move_fcf"] = curr_fcf + beg_fcf - last_fcf
+    ratio["capital"] = (
+        (curr["2100"] or 0)
+        + (curr["2110"] or 0)
+        + (curr["2280"] or 0)
+        + (curr["2320"] or 0)
+        + (curr["2530"] or 0)
+        + (curr["2540"] or 0)
+        + (curr["2580"] or 0)
+        + curr["3XXX"]
+    )
+    ratio["return_of_capital"] = ratio["move_fcf"] * 100 / ratio["capital"]
+
+    return ratio
+
+
+def calculate_company_latest_ratio(company_id):
+    """This function calculate financial ratios for the last 3 years."""
+
+    now = datetime.now()
+    year = now.year
+    quarter = now.month // 3
+    if quarter == 0:
+        year = year - 1
+        quarter = 4
+
+    reports = report_table.query(
+        Select="ALL_ATTRIBUTES",
+        KeyConditionExpression=boto3.dynamodb.conditions.Key("company_id").eq(
+            company_id
+        ),
+    )["Items"]
+
+    curr_ratio = calculate_company_ratio(
+        next(
+            r for r in reports if r["year_quauter"] == (str(year) + "Q" + str(quarter))
+        ),
+        next(r for r in reports if r["year_quauter"] == (str(year - 1) + "Q4")),
+        next(
+            r
+            for r in reports
+            if r["year_quauter"] == (str(year - 1) + "Q" + str(quarter))
+        ),
+    )
+    last_ratio = calculate_company_ratio(
+        next(
+            r
+            for r in reports
+            if r["year_quauter"] == (str(year - 1) + "Q" + str(quarter))
+        ),
+        next(r for r in reports if r["year_quauter"] == (str(year - 2) + "Q4")),
+        next(
+            r
+            for r in reports
+            if r["year_quauter"] == (str(year - 2) + "Q" + str(quarter))
+        ),
+    )
+    last2_ratio = calculate_company_ratio(
+        next(
+            r
+            for r in reports
+            if r["year_quauter"] == (str(year - 2) + "Q" + str(quarter))
+        ),
+        next(r for r in reports if r["year_quauter"] == (str(year - 3) + "Q4")),
+        next(
+            r
+            for r in reports
+            if r["year_quauter"] == (str(year - 3) + "Q" + str(quarter))
+        ),
+    )
+
+    if curr_ratio is None or last_ratio is None or last2_ratio is None:
+        return
+
+    curr_ratio["last_return_of_capital"] = last_ratio["return_of_capital"]
+    curr_ratio["last2_return_of_capital"] = last2_ratio["return_of_capital"]
+    curr_ratio["avg3_return_of_capital"] = (
+        (
+            (curr_ratio["return_of_capital"] / 100 + 1)
+            * (last_ratio["return_of_capital"] / 100 + 1)
+            * (last2_ratio["return_of_capital"] / 100 + 1)
+        )
+        ** (1 / 3)
+        - 1
+    ) * 100
+
+    ratio_table.put_item(Item=curr_ratio)
+
+
 def update_company_process_time(company_id):
-    """ This function updates the process time of company """
+    """This function updates the process time of company"""
 
     company_table.update_item(
-        Key = { 'company_id': company_id },
-        UpdateExpression = 'set process_time = :process_time',
-        ExpressionAttributeValues = { ':process_time': Decimal(time.time()) }
+        Key={"company_id": company_id},
+        UpdateExpression="set process_time = :process_time",
+        ExpressionAttributeValues={
+            ":process_time": Decimal(datetime.now().timestamp())
+        },
     )
 
 
-def lambda_handler(event, context): # pylint: disable=unused-argument
-    """ This is lambda handler. """
+def lambda_handler(event, context):  # pylint: disable=unused-argument
+    """This is lambda handler."""
 
     company_to_run = get_company_to_run()
-    reports = get_report_records(company_to_run['company_id'])
-    report_paths = get_report_paths(company_to_run['company_id'], reports)
+    reports = get_report_records(company_to_run["company_id"])
+    report_paths = get_report_paths(company_to_run["company_id"], reports)
     if len(report_paths) > 0:
-        get_report(company_to_run['company_id'], report_paths)
+        get_report(company_to_run["company_id"], report_paths)
+        calculate_company_latest_ratio(company_to_run["company_id"])
 
-    update_company_process_time(company_to_run['company_id'])
+    update_company_process_time(company_to_run["company_id"])
+
 
 lambda_handler(None, None)
--- lambda/get_listing_companies.py
+++ lambda/get_listing_companies.py
@@ -4,69 +4,66 @@ import boto3
 import requests
 from bs4 import BeautifulSoup
 
-TABLE_NAME = 'valuation-sam-ListingCompanyTable-LDPLI51PXXDP'
-dynamodb = boto3.resource('dynamodb')
+TABLE_NAME = "valuation-sam-ListingCompanyTable-LDPLI51PXXDP"
+dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(TABLE_NAME)
-URL = 'https://isin.twse.com.tw/isin/C_public.jsp?strMode='
-strMode = ['2', '4']
+URL = "https://isin.twse.com.tw/isin/C_public.jsp?strMode="
+strMode = ["2", "4"]
 
-def lambda_handler(event, context): # pylint: disable=unused-argument
-    """ This is lambda handler. """
 
-    companies = { company['company_id']: company for company in table.scan()['Items'] }
+def lambda_handler(event, context):  # pylint: disable=unused-argument
+    """This is lambda handler."""
+
+    companies = {company["company_id"]: company for company in table.scan()["Items"]}
 
     for mode in strMode:
-        resp = requests.get(URL + mode, timeout = 30)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        trs = soup.select('tr')
+        resp = requests.get(URL + mode, timeout=30)
+        soup = BeautifulSoup(resp.text, "html.parser")
+        trs = soup.select("tr")
         for tr in trs:
             if len(tr.find_all()) == 7:
-                id_name = tr.select_one(':nth-child(1)').get_text().split()
+                id_name = tr.select_one(":nth-child(1)").get_text().split()
                 if len(id_name) < 2:
                     continue
 
                 co_id = id_name[0]
                 name = id_name[1]
-                industry = tr.select_one(':nth-child(5)').get_text()
-                cfi_code = tr.select_one(':nth-child(6)').get_text()
+                industry = tr.select_one(":nth-child(5)").get_text()
+                cfi_code = tr.select_one(":nth-child(6)").get_text()
 
-                if cfi_code != 'ESVUFR' or name.find('KY') >= 0 or industry == '金融保險業':
+                if (
+                    cfi_code != "ESVUFR"
+                    or name.find("KY") >= 0
+                    or industry == "金融保險業"
+                ):
                     continue
 
                 company = companies.get(co_id)
                 if company is None:
                     table.put_item(
-                        Item = {
-                            'company_id': co_id,
-                            'company_name': name,
-                            'industry': industry,
-                            'process_time': 0
+                        Item={
+                            "company_id": co_id,
+                            "company_name": name,
+                            "industry": industry,
+                            "process_time": 0,
                         }
                     )
                 else:
-                    if name != company['company_name'] or industry != company['industry']:
+                    if (
+                        name != company["company_name"]
+                        or industry != company["industry"]
+                    ):
                         table.update_item(
-                            Key = {
-                                'company_id': co_id
+                            Key={"company_id": co_id},
+                            AttributeUpdates={
+                                "company_name": {"Value": name, "Action": "PUT"},
+                                "industry": {"Value": industry, "Action": "PUT"},
                             },
-                            AttributeUpdates = {
-                                'company_name': {
-                                    'Value': name,
-                                    'Action': 'PUT'
-                                },
-                                'industry': {
-                                    'Value': industry,
-                                    'Action': 'PUT'
-                                }
-                            }
                         )
                     companies.pop(co_id)
 
     for company in companies:
-        table.delete_item(
-            Key = {
-                'company_id': company['company_id']
-            }
-        )
+        table.delete_item(Key={"company_id": company["company_id"]})
+
 
 lambda_handler(None, None)
